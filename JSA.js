302366da2d1e490375c8217005ca255812b10e9c
Alcester Ayala <alcestergif@gmail.com>
2023-04-25 12:06:55 -0400

    commit

--- JSA.js
+++ JSA.js
@@ -2,9 +2,12 @@
 var firstName = "Alcester";
 var lastName = "Ayala";
 var age = 21;
+
 // full name alert
 alert ("My name is " + firstName + " " + lastName);
+
 // age alert
 alert ("I am " + age + " years old");
+
 // Console log fun message
 console.log ("im having so much fun in this class");
