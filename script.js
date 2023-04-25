05361c10f96e5e2a6975c5c5742305a3a18f37cf
Alcester Ayala <alcestergif@gmail.com>
2023-04-24 23:50:29 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Declare variables
+var firstName = "Alcester";
+var lastName = "Ayala";
+var age = 21;
+// full name alert
+alert ("My name is " + firstName + " " + lastName);
+// age alert
+alert ("I am " + age + " years old");
+// Console log fun message
+console.log ("im having so much fun in this class");
