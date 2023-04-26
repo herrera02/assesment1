ff8fb20579ceb95950f9f3c5648d41998a0fa04d
Alcester Ayala <alcestergif@gmail.com>
2023-04-25 22:29:35 -0400

    init

---
+++ loopingandarray.js
@@ -0,0 +1,21 @@
+// 0-1000
+for (let i = 1000; i >= 0; i--) 
+if (i % 2 === 0) 
+   console.log(i);
+
+
+// 1000-0
+for (let i = 0; i <= 10000; i++) {
+    if (i === 2500) {
+        alert("A quarter of the way there!");
+    } else if (i === 5000) {
+        alert("Halfway there!");
+    } else if (i === 10000) {
+        alert("The loop is done!");
+    }
+}
+// Top 5 favorite 
+const favoriteTVShows = ['Breaking Bad', 'The Office', 'Game of Thrones', 'Stranger Things', 'The Crown'];
+for (let i = 0; i < favoriteTVShows.length; i++) {
+    console.log("My #" + (i+1) + " favorite tv show is " + favoriteTVShows[i]);
+}
