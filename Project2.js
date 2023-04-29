ffd6c5f4f8a69ce9f7e6ccb8f94ceea98e3a7f67
Alcester Ayala <alcestergif@gmail.com>
2023-04-29 00:06:45 -0400

    init

---
+++ Project2.js
@@ -0,0 +1,73 @@
+let wins = 0;
+let ties = 0;
+let losses = 0;
+
+function computerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
+}
+
+function playRound(userChoice) {
+
+  const computer = computerChoice();
+
+  let result;
+
+  if (userChoice === "rock") {
+
+    if (computer === "scissors") {
+
+      result = "win";
+    } else if (computer === "paper") {
+    
+      result = "lose";
+      
+    } else {
+      result = "tie";
+    }
+  } else if (userChoice === "paper") {
+    if (computer === "rock") {
+      result = "win";
+
+      result = "lose";
+
+    } else {
+      result = "tie";}
+
+  } else if (userChoice === "scissors") {
+    if (computer === "paper") {
+      result = "win";
+    } else if (computer === "rock") {
+
+      result = "lose";
+    } else {
+      result = "tie";}
+
+  }
+  if (result === "win") {
+    wins++;
+  } else if (result === "lose") {
+    losses++;
+  } else {
+    ties++;
+  }
+  document.getElementById("wins").innerHTML = wins;
+
+  document.getElementById("ties").innerHTML = ties;
+
+  document.getElementById("losses").innerHTML = losses;
+
+}
+
+document.getElementById("rock").addEventListener("click", function() {
+  playRound("rock");
+});
+
+document.getElementById("paper").addEventListener("click", function() {
+  playRound("paper");
+});
+
+document.getElementById("scissors").addEventListener("click", function() {
+  playRound("scissors");
+});
