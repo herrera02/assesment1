4ecd7774e90925d42a949a1cdf62efb719516d6f
Alcester Ayala <alcestergif@gmail.com>
2023-04-29 11:53:01 -0400

    init

---
+++ KeyboardEvent.js
@@ -0,0 +1,17 @@
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+let secretLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+document.body.addEventListener('keydown', function(event) {
+    
+  if (event.key === secretLetter) {
+
+    const message = document.createElement('p');
+
+    message.textContent = `secret key ${secretLetter.toUpperCase()} PRESSED`;
+
+    document.body.appendChild(message);
+
+    secretLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    }});
